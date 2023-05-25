f7b765442fa877515bbb9ddd0fe6643173110e05
Habibullah Howlader <habibullah.howlader@bjitacademy.com>
2023-05-25 14:26:14 +0600

    added Getway-api service

---
+++ productApp/src/main/java/com/Habib/productApp/config/HelloController.java
@@ -0,0 +1,18 @@
+package com.Habib.productApp.config;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+public class HelloController {
+    Logger logger = Logger.getLogger("HelloController");
+
+    @GetMapping("/hello")
+    public String hello (){
+        logger.info("Hello");
+        return "Hello service";
+    }
+}
