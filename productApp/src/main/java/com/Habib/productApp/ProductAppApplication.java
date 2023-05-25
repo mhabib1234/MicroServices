c58d2121aa49a20f401fc03542a20a3c5ed49e14
Habibullah Howlader <habibullah.howlader@bjitacademy.com>
2023-05-25 10:17:21 +0600

    added and implement Eureka, Config Server, product -app, all are woking fine

---
+++ productApp/src/main/java/com/Habib/productApp/ProductAppApplication.java
@@ -0,0 +1,23 @@
+package com.Habib.productApp;
+
+import com.Habib.productApp.config.MigrationConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAppApplication implements CommandLineRunner {
+
+	@Autowired
+	private MigrationConfiguration migrationConfiguration;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAppApplication.class, args);
+	}
+
+	public void run (String... args) throws Exception{
+		System.out.println("migration Configuration "+ migrationConfiguration.getMigration());
+	}
+
+}
